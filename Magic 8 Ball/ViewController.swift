a2d0dbbd384e206e042f9f6582fafb194fb08010
Halil Eren Şahin <halilerensahin@gmail.com>
2025-04-04 17:03:37 +0300

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Eren on 4.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Magic 8 BallTests/Magic_8_BallTests.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_8_BallTests.swift
+//  Magic 8 BallTests
+//
+//  Created by Eren on 4.04.2025.
+//
+
+import Testing
+@testable import Magic_8_Ball
+
+struct Magic_8_BallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
